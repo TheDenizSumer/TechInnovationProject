9ba1468cb0f2862a1a3e55c57e9a42015f6e0de7
School Computer <phishsquish@gmail.com>
2023-09-27 09:52:28 -0700

    9/27 Added centroid and fixed defgrad bug

--- strainMapping/deformationGradientFunction.py
+++ strainMapping/deformationGradientFunction.py
@@ -2,6 +2,10 @@ import math
 from sympy import symbols, diff
 import numpy as np
 
+def centroid(p1, p2, p3, p4):
+    x = (p1[0]+p2[0]+p3[0]+p4[0])/4
+    y = (p1[1]+p2[1]+p3[1]+p4[1])/4
+    return x, y
 
 def F(transposed, origin):
 
@@ -16,20 +20,28 @@ def F(transposed, origin):
 
     v = (transposed[0][1]-origin[0][1])*(1/(4*a1_2*b4_1))*(x-origin[1][0])*(y-origin[3][1]) + (transposed[1][1]-origin[1][1])*(-1*(1/(4*a1_2*b2_3))*(x-origin[0][0])*(y-origin[2][1])) + (transposed[2][1]-origin[2][1])*(1/(4*a4_3*b2_3))*(x-origin[3][0])*(y-origin[1][1]) + (transposed[3][1]-origin[3][1])*(-1*(1/(4*a4_3*b4_1))*(x-origin[2][0])*(y-origin[0][1]))
 
-    #u(x, y) = function of x & y returns zrelative x displacment in element
-    #v(x, y) = function of x & y returns relative x displacment in element
-    
-    xx = float(diff(u, x))
-    xy = float(diff(u, y))
-    yx = float(diff(v, x))
-    yy = float(diff(v, y))
+    #u(x, y) = function of x & y returns relative x displacment in element
+    #v(x, y) = function of x & y returns relative y displacment in element
+
+    #Tx, Ty = centroid(transposed[0], transposed[1], transposed[2], transposed[3])
+    tx, ty = centroid(origin[0], origin[1], origin[2], origin[3])
+
+    xx = float(diff(u, x).replace(y, ty))
+    xy = float(diff(u, y).replace(x, tx))
+    yx = float(diff(v, x).replace(y, ty))
+    yy = float(diff(v, y).replace(x, tx))
 
     F = np.matrix([[xx, xy], [yx, yy]])
     
-    return F
+    #xx = float(diff(u, x).replace(y, Ty))
+    #xy = float(diff(u, y).replace(x, Tx))
+    #yx = float(diff(v, x).replace(y, Ty))
+    #yy = float(diff(v, y).replace(x, Tx))
+    #FT = np.matrix([[xx, xy], [yx, yy]])
+    return F, tx, ty
 
 
-X = [
+'''X = [
     [896, 386], 
     [947, 383], 
     [947, 330], 
@@ -40,11 +52,11 @@ x = [
     [947, 384], 
     [946, 331], 
     [897, 332]
-    ]
+    ]'''
 
 
 
-square_undeformed_green = [
+'''square_undeformed_green = [
 [1, 1],
 [5, 1],
 [5, 5],
@@ -90,11 +102,11 @@ square_deformed = [
 [10, 8],
 [8, 10],
 [6, 8]
-]
+]'''
 
 
 
-
+'''
 print(f'Blue square : \n{F(square_deformed_blue, square_undeformed_green)}')
 print()
 print(f'Red square : \n{F(square_deformed_red, square_undeformed_green)}')
@@ -106,20 +118,21 @@ print()
 print(f'Purple square : \n{F(square_deformed_purple, square_undeformed_green)}')
 
 print()
-print(f'sdjdnkajs square : \n{F(square_undeformed_green, square_deformed_red)}')
+print(f'sdjdnkajs square : \n{F(square_undeformed_green, square_deformed_red)}')'''
 
-
+'''
 X = [
     [896, 386], 
     [947, 383], 
     [947, 330], 
     [897, 330]
-    ]
+    ]#transposed
 x = [
     [896, 387], 
     [947, 384], 
-    [946, 331], 
+    [926, 331], 
     [897, 332]
-    ]
+    ]#transposed
 
 print(F(X, x))
+'''
